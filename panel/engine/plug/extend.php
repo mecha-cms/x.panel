0d91c30e10c657d5c74ca811c0875885a46051a5
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-01-27 21:24:45 +0700

    #3
    https://github.com/mecha-cms/genome/issues/3

---
+++ panel/engine/plug/extend.php
@@ -0,0 +1,22 @@
+<?php
+
+Extend::plug('info', function($id) use($config, $language, $url, $__state) {
+    $f = EXTEND . DS . $id . DS;
+    return new Page(File::exist([
+        // Check whether the localized “about” file is available
+        $f . 'about.' . $config->language . '.page',
+        // Use the default “about” file if available
+        $f . 'about.page'
+    ], null), [
+        'id' => Folder::exist($f) ? $id : null,
+        'title' => To::title($id),
+        'author' => $language->anonymous,
+        'version' => '0.0.0',
+        'content' => $language->_message_avail($language->description),
+        'url' => $url . '/' . $__state->path . '/::g::/extend/' . $id
+    ], __c2f__('Extend'));
+});
+
+Extend::plug('version', function($id, $v = null) {
+    return Mecha::version($v, Extend::info($id)->version);
+});
