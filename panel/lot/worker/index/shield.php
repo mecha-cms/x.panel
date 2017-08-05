78d42676869c030a58a2b79835b7c14f1581e057
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-08-05 23:13:48 +0700

    ditto

--- panel/lot/worker/index/shield.php
+++ panel/lot/worker/index/shield.php
@@ -2,12 +2,14 @@
 
 Config::set('panel.v.' . $__chops[0] . '.as', $config->shield);
 
-Hook::set('panel.a.' . $__chops[0], function($__a, $__v) use($language, $__chops) {
-    if (file_exists(LOT . DS . $__chops[0] . DS . $__v[0]->slug . DS . 'state' . DS . 'config.php')) {
-        $__a = ['state' => [$language->setting, $__a['edit'][1] . '/state/config.php']] + $__a;
-    }
-    $__a['edit'][0] = $language->open;
-    return $__a;
-});
+if (count($__chops) === 1) {
+    Hook::set('panel.a.' . $__chops[0], function($__a, $__v) use($language, $__chops) {
+        if (file_exists(LOT . DS . $__chops[0] . DS . $__v[0]->slug . DS . 'state' . DS . 'config.php')) {
+            $__a = ['state' => [$language->setting, $__a['edit'][1] . '/state/config.php' . HTTP::query(['token' => false, 'force' => false])]] + $__a;
+        }
+        $__a['edit'][0] = $language->open;
+        return $__a;
+    });
+}
 
 require __DIR__ . DS . 'extend.php';
