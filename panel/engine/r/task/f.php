0644bd243f607a7e0653dd2c20da7905b18e20b1
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-06-09 21:29:34 +0700

    Update

--- panel/engine/r/task/f.php
+++ panel/engine/r/task/f.php
@@ -4,14 +4,14 @@ $GLOBALS['_']['fn'] = $fn = \array_shift($_['chops']);
 $GLOBALS['_']['chops'] = $_['chops'];
 $GLOBALS['_']['f'] = \stream_resolve_include_path(\LOT . \DS . \implode(\DS, $_['chops'])) ?: null;
 
+$lot = \e($GLOBALS['_' . ($_SERVER['REQUEST_METHOD'] ?? 'GET')] ?? []);
 if (\is_file($_f = __DIR__ . \DS . 'f' . \DS . $fn . '.php')) {
-    $_ = (function($_f) {
+    $_ = (function($_f, $lot) {
         extract($GLOBALS, \EXTR_SKIP);
-        $lot = \e($GLOBALS['_' . ($_SERVER['REQUEST_METHOD'] ?? 'GET')] ?? []);
         return require $_f;
-    })($_f);
+    })($_f, $lot);
 } else if (\function_exists($fn = "\\_\\lot\\x\\panel\\task\\f\\" . $fn)) {
-    $_ = \call_user_func($fn, $_, \e($GLOBALS['_' . ($_SERVER['REQUEST_METHOD'] ?? 'GET')] ?? []));
+    $_ = \call_user_func($fn, $_, $lot);
 }
 
 $GLOBALS['_'] = $_; // Update data
