a61f828403dfdc9b373b7be65c600f16e095163c
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-01-02 19:46:24 +0700

    Update

--- panel/index/panel/type/file.php
+++ panel/index/panel/type/file.php
@@ -96,7 +96,7 @@ $bar = [
                 'set' => [
                     'description' => ['New %s', 'File'],
                     'icon' => 'M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z',
-                    'skip' => 's' === $_['task'],
+                    'skip' => 'set' === $_['task'],
                     'stack' => 10.5,
                     'title' => false,
                     'url' => x\panel\to\link([
